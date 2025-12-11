406d57d9bea3b419f662613adb7955cce735b977
mannammahendra <mahendramannam865@gmail.com>
2025-12-12 00:24:13 +0530

    added main theme

---
+++ kumi/comp.tsx
@@ -0,0 +1,16 @@
+import React,{component} from 'react';
+import {View, Text, ScrollView} from 'react-native';
+
+export obj extends Component {
+    render() {
+        return (
+            <ScrollView>
+            <Text> Welcome to the react app</Text>
+            <Text> This project aims at </Text>
+            <Text> Implementation the userr of react comp</Text>
+            <Text> Components in the chat based appli</Text>
+            <Text> Applications for the real-world</Text>
+            </ScrollView>
+        )
+    }
+}
